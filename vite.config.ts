5f2bf7478f0ed4689c28245dbd74cadc4ec788ee
vaexenc <53697802+vaexenc@users.noreply.github.com>
2023-08-03 23:36:18 +0200

    minify production index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import injectHtmlPlugin from "./injectHtmlPlugin";
+import {ViteMinifyPlugin} from "vite-plugin-minify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), injectHtmlPlugin()]
+	plugins: [react(), injectHtmlPlugin(), ViteMinifyPlugin()]
 });
