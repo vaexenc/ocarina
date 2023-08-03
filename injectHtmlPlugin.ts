5cda6ce3737ee07adb57dd49b220616a5949db9f
vaexenc <53697802+vaexenc@users.noreply.github.com>
2023-08-03 23:06:57 +0200

    inject time stamp into <noscript> content during build time

---
+++ injectHtmlPlugin.ts
@@ -0,0 +1,21 @@
+function currentTimeStamp() {
+	const now = new Date();
+
+	const year = now.getUTCFullYear();
+	const month = String(now.getUTCMonth() + 1).padStart(2, "0");
+	const day = String(now.getUTCDate()).padStart(2, "0");
+	const hours = String(now.getUTCHours()).padStart(2, "0");
+	const minutes = String(now.getUTCMinutes()).padStart(2, "0");
+	const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+export default function injectHtmlPlugin() {
+	return {
+		name: "inject-html-plugin",
+		transformIndexHtml(html) {
+			return html.replaceAll("$inject:date", currentTimeStamp());
+		}
+	};
+}
