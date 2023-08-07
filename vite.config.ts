ff8e46006d6a463e0582fd52119f877e3279bd10
vaexenc <53697802+vaexenc@users.noreply.github.com>
2023-08-07 20:50:13 +0200

    enable absolute imports for tsx files

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import injectHtmlPlugin from "./injectHtmlPlugin";
 import {ViteMinifyPlugin} from "vite-plugin-minify";
+import {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), injectHtmlPlugin(), ViteMinifyPlugin()]
+	plugins: [react(), injectHtmlPlugin(), ViteMinifyPlugin()],
+	resolve: {
+		alias: {
+			"/": resolve(__dirname)
+		}
+	}
 });
