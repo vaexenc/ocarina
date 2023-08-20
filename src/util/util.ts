2ffd4288b521907fe3949c434e09cd37569e2077
vaexenc <53697802+vaexenc@users.noreply.github.com>
2023-08-20 16:30:21 +0200

    add checkIfMobileDevice()

--- src/util/util.ts
+++ src/util/util.ts
@@ -19,3 +19,12 @@ export function map(
 export function clamp(value: number, min: number, max: number) {
 	return Math.min(Math.max(value, min), max);
 }
+
+export function checkIfMobileDevice() {
+	return (
+		/Mobile|Android|iOS|iPhone|iPad|Windows Phone/i.test(navigator.userAgent) ||
+		"ontouchstart" in window ||
+		navigator.maxTouchPoints > 0 ||
+		window.innerWidth <= 768
+	);
+}
