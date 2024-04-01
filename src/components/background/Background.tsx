87c6352fa0afd73dff61fa531cf3d02b774ca0c1
vaexenc <53697802+vaexenc@users.noreply.github.com>
2024-04-01 19:22:22 +0200

    rework bg parallax to be based on js instead of css
    this fix avoids css transition bugginess on firefox and also adds 16 kb to the final bundle size while providing no other noticeable differences yippieeee

--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -1,5 +1,5 @@
-import clsx from "clsx";
-import {useCallback, useEffect, useRef} from "react";
+import {animated, useSpring} from "@react-spring/web";
+import {useCallback, useEffect} from "react";
 import style from "./Background.module.scss";
 import {clamp} from "/src/util/util";
 
@@ -10,22 +10,27 @@ export default function Background({
 	isParallaxOn: boolean;
 	doesParallaxUpdate: boolean;
 }) {
-	const backgroundRoot = useRef<HTMLDivElement>(null);
+	const [springs] = useSpring(
+		() => ({
+			x: 0,
+			y: 0,
+			config: {
+				mass: 1,
+				friction: 50,
+				tension: 100,
+			},
+		}),
+		[]
+	);
 
 	const handleMouseMove = useCallback(
 		(event: MouseEvent) => {
 			if (!isParallaxOn || !doesParallaxUpdate) return;
 
-			backgroundRoot.current?.style.setProperty(
-				"--offset-x",
-				String(clamp((event.clientX / window.innerWidth) * 2 - 1, -1, 1))
-			);
-			backgroundRoot.current?.style.setProperty(
-				"--offset-y",
-				String(clamp((event.clientY / window.innerHeight) * 2 - 1, -1, 1))
-			);
+			springs.x.start(clamp((event.clientX / window.innerWidth) * 2 - 1, -1, 1));
+			springs.y.start(clamp((event.clientY / window.innerHeight) * 2 - 1, -1, 1));
 		},
-		[isParallaxOn, doesParallaxUpdate]
+		[isParallaxOn, doesParallaxUpdate, springs.x, springs.y]
 	);
 
 	useEffect(() => {
@@ -37,34 +42,31 @@ export default function Background({
 	}, [handleMouseMove]);
 
 	return (
-		<>
-			<div
-				className={clsx(style.bg, {[style["no-parallax"]]: !isParallaxOn})}
-				ref={backgroundRoot}
-				style={
-					!isParallaxOn ? ({"--offset-x": 0, "--offset-y": 0} as React.CSSProperties) : {}
-				}
-			>
-				<img className="bg-elem bg-sky" src="images/background/bg-sky.webp" />
-				<img className="bg-elem bg-mountains" src="images/background/bg-mountains.webp" />
-				<img
-					className="bg-elem bg-death-mountain"
-					src="images/background/bg-death-mountain.webp"
-				/>
-				<img
-					className="bg-elem bg-river-castle"
-					src="images/background/bg-river-castle.webp"
-				/>
-				<img
-					className="bg-elem bg-lon-lon-ranch"
-					src="images/background/bg-lon-lon-ranch.webp"
-				/>
-				<img
-					className="bg-elem bg-bottom-right"
-					src="images/background/bg-bottom-right.webp"
-				/>
-				<img className="bg-elem bg-bottom" src="images/background/bg-bottom.webp" />
-			</div>
-		</>
+		<animated.div
+			className={style.bg}
+			style={
+				!isParallaxOn
+					? ({"--offset-x": 0, "--offset-y": 0} as React.CSSProperties)
+					: ({
+							"--offset-x": springs.x.to((value) => value.toString()),
+							"--offset-y": springs.y.to((value) => value.toString()),
+							// eslint-disable-next-line no-mixed-spaces-and-tabs
+					  } as React.CSSProperties)
+			}
+		>
+			<img className="bg-elem bg-sky" src="images/background/bg-sky.webp" />
+			<img className="bg-elem bg-mountains" src="images/background/bg-mountains.webp" />
+			<img
+				className="bg-elem bg-death-mountain"
+				src="images/background/bg-death-mountain.webp"
+			/>
+			<img className="bg-elem bg-river-castle" src="images/background/bg-river-castle.webp" />
+			<img
+				className="bg-elem bg-lon-lon-ranch"
+				src="images/background/bg-lon-lon-ranch.webp"
+			/>
+			<img className="bg-elem bg-bottom-right" src="images/background/bg-bottom-right.webp" />
+			<img className="bg-elem bg-bottom" src="images/background/bg-bottom.webp" />
+		</animated.div>
 	);
 }
